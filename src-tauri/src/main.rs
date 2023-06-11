2d4d57cf596c00b604e4e7f60a0aa7e30b1eab85
Soryn Bächli <soryn.baechli@gmail.com>
2023-06-11 21:13:27 +0200

    Finally able to read vcp features and data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// use ddc_hi::{traits::Ddc, Display};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,27 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // for mut display in Display::enumerate() {
+    //     display.update_capabilities().unwrap();
+
+    //     println!(
+    //         "{:?} {}: {:?} {:?}",
+    //         display.backend(),
+    //         display.id,
+    //         display.info().manufacturer_id,
+    //         display.info().model_name
+    //     );
+
+    //     if let Some(feature) = display.capabilities {
+    //         match feature.ty {
+    //             Some()
+    //         }
+
+    //         let value = display.handle.get_vcp_feature(0x10);
+    //         println!("{:?}", value);
+    //     }
+    // }
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
